9bb2320f579cc4b30e86f2af2ea4a006161e725c
shadowDarsan <panerusamrat@gmail.com>
2023-03-03 19:07:13 +0545

    routes and controller created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
