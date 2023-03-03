0765f6c4c5f74f1f444e928957bd82b40a9b2326
shadow <panerusamrat@gmail.com>
2023-03-03 23:31:18 +0545

    created register functionalities with hash

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,15 +1,48 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 
 // @desc Register a new user
 // @route /api/users
 // access Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { kage, email, password } = req.body
+  const { name, email, password } = req.body
 
   // validation
-  if (!kage || !email || !password) {
+  if (!name || !email || !password) {
     res.status(400)
     throw new Error('Please Include all fileds')
+  }
+
+  // Find if user already Exists
+
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User Already Exists')
+  }
+
+  //Hash Password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create User
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
   }
 })
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: [true, 'Please add a name'] },
+    email: {
+      type: String,
+      required: [true, 'Please add a email'],
+      unique: true,
+    },
+    password: { type: String, required: [true, 'Please add a password'] },
+    isAdmin: { type: String, required: true, default: false },
+  },
+  { timestamp: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
