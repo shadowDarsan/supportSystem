36ac672257faa29af3d12089c066c54401a18ac4
shadow <panerusamrat@gmail.com>
2023-03-03 23:08:32 +0545

    created middleware and database connection Sting

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,9 +1,24 @@
-const registerUser = (req, res) => {
-  res.send('Register Route')
-}
-const loginUser = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+// @desc Register a new user
+// @route /api/users
+// access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { kage, email, password } = req.body
+
+  // validation
+  if (!kage || !email || !password) {
+    res.status(400)
+    throw new Error('Please Include all fileds')
+  }
+})
+
+// @desc Login a user
+// @route /api/users/login
+// access Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route')
-}
+})
 
 module.exports = {
   registerUser,
